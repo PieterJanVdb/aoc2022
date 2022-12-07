79771a0543fc7889546e2a21c3f77125bf4f17c0
Pieter-Jan Vandenbussche <pieter-jan.vandenbussche@inthepocket.com>
2022-12-07 18:39:23 +0100

    Woops

--- src/aoc2022/day6/core.clj
+++ src/aoc2022/day6/core.clj
@@ -2,13 +2,13 @@
 
 (def input (slurp "src/aoc2022/day6/input.txt"))
 
-(defn start-of-message [marker-length]
+(defn start-of-message [marker-n]
   (first
-      (filter
-       #(apply
-         distinct?
-         (subvec (vec input) % (+ marker-length %)))
-       (range 4 (count input)))) marker-length)
+   (filter
+    #(apply
+      distinct?
+      (subvec (vec input) (- % marker-n) %))
+    (range marker-n (count input)))))
 
 (def part1 (start-of-message 4))
 (def part2 (start-of-message 14))
