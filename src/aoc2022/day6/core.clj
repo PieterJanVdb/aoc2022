a09c5d242e5985234c9cbc1d7ddd076ba125c6ec
Pieter-Jan Vandenbussche <pieter-jan.vandenbussche@inthepocket.com>
2022-12-07 18:38:57 +0100

    Start from marker-n

--- src/aoc2022/day6/core.clj
+++ src/aoc2022/day6/core.clj
@@ -3,12 +3,12 @@
 (def input (slurp "src/aoc2022/day6/input.txt"))
 
 (defn start-of-message [marker-length]
-  (+ (first
+  (first
       (filter
        #(apply
          distinct?
          (subvec (vec input) % (+ marker-length %)))
-       (range (count input)))) marker-length))
+       (range 4 (count input)))) marker-length)
 
 (def part1 (start-of-message 4))
 (def part2 (start-of-message 14))
